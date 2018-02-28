1c745fe95bf3f5900d3d6dff427e0fc4dd2d3d38
Victor <wangzhevictor1983@outlook.com>
2018-02-28 22:35:14 +0800

    add routing config

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login.component';
+
+const appRoutes: Routes = [
+  { path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -10,7 +19,11 @@ import { LoginComponent } from './components/login.component';
     LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
